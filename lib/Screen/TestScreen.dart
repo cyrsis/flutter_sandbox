5483ccc060a14ff72cc1b57cf5c6d3304e9fc777
cyrsis <em3888@gmail.com>
2018-03-27 19:03:05 +0800

    Working state

--- lib/Screen/TestScreen.dart
+++ lib/Screen/TestScreen.dart
@@ -14,35 +14,23 @@ import 'package:flutter_sandbox/Widgets/VerticalDivider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_sandbox/Widgets/QuickAction.dart';
 import 'package:fluttery/framing.dart';
+import 'package:carousel/carousel.dart';
 
 class TestScreen extends StatelessWidget {
   final profile = getProfile();
 
   @override
   Widget build(BuildContext context) {
-    return new Column(children: <Widget>[
-      new Padding(
-        padding: const EdgeInsets.only(top: 100.0),
+    return new SizedBox(
+      height: 240.0,
+      child: new Carousel(
+        children: [
+          new NetworkImage(
+              'https://pbs.twimg.com/profile_images/760249570085314560/yCrkrbl3_400x400.jpg'),
+          new NetworkImage(
+              'https://webinerds.com/app/uploads/2017/11/d49396_d9c5d967608d4bc1bcf09c9574eb67c9-mv2.png')
+        ].map((netImage) => new Image(image: netImage)).toList(),
       ),
-      new Text('Whatever', softWrap: true, style: AppStyle.AppBarTextStyle),
-      new Container(
-          child: new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-            new Text(
-              'Babe',
-              style: AppStyle.AppBarTextStyle,
-            ),
-            new Text(
-              'I miss you',
-              style: AppStyle.AppBarTextStyle,
-            ),
-            new FloatingActionButton(
-              onPressed: () => Navigator.of(context).pushNamed('/webview'),
-              tooltip: 'Increment',
-              child: new Icon(Icons.add),
-            )
-          ]))
-    ]);
+    );
   }
 }
