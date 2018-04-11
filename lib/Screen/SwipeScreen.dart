45271bf5e5aae08bcb0de1a638bbeab34a28a04b
cyrsis <em3888@gmail.com>
2018-04-11 16:05:53 +0800

    come back work on this

--- lib/Screen/SwipeScreen.dart
+++ lib/Screen/SwipeScreen.dart
@@ -1,3 +1,4 @@
+//https://gist.githubusercontent.com/swat-cat/63797845dcd2bf6adc868c0daafd71f5/raw/491cd4b1be8b18d6ea8da89a2cd6a6eefd38f4df/slide.dart
 import 'package:flutter/material.dart';
 
 class SwipeScreen extends StatefulWidget {
@@ -5,7 +6,8 @@ class SwipeScreen extends StatefulWidget {
   _SwipeScreenState createState() => new _SwipeScreenState();
 }
 
-class _SwipeScreenState extends State<SwipeScreen> with TickerProviderStateMixin {
+class _SwipeScreenState extends State<SwipeScreen>
+    with TickerProviderStateMixin {
   AnimationController _scaleController;
 
   AnimationController _showEasyController;
@@ -23,28 +25,31 @@ class _SwipeScreenState extends State<SwipeScreen> with TickerProviderStateMixin
   @override
   void initState() {
     super.initState();
-    _scaleController = new AnimationController(duration: const Duration(milliseconds:800 ) ,vsync: this);
+    _scaleController = new AnimationController(
+        duration: const Duration(milliseconds: 800), vsync: this);
     _showEasyController = new AnimationController(
         duration: const Duration(milliseconds: 20000), vsync: this);
-    
-    _showEasy = new Tween(begin: new Offset(0.0, 0.3),end: new Offset(0.0, 0.0))
-                 .animate(
-                 new CurvedAnimation(parent: _showEasyController, curve: new Interval(0.0, 0.5,curve: Curves.easeInOut), ));
 
-    _showEasy.addStatusListener((status){
-      if (status == AnimationStatus.completed)
-        _scaleController.forward();
-      if (status == AnimationStatus.dismissed)
-        _scaleController.reverse();
+    _showEasy =
+        new Tween(begin: new Offset(0.0, 0.3), end: new Offset(0.0, 0.0))
+            .animate(new CurvedAnimation(
+      parent: _showEasyController,
+      curve: new Interval(0.0, 0.5, curve: Curves.easeInOut),
+    ));
+
+    _showEasy.addStatusListener((status) {
+      if (status == AnimationStatus.completed) _scaleController.forward();
+      if (status == AnimationStatus.dismissed) _scaleController.reverse();
       setState(() {
-        _showHint= false;
-        _isMenuAvailable =true;
+        _showHint = false;
+        _isMenuAvailable = true;
       });
     });
     _frontScale = new Tween(
       begin: 1.0,
       end: 0.0,
-    ).animate(new CurvedAnimation(
+    )
+        .animate(new CurvedAnimation(
       parent: _scaleController,
       curve: new Interval(0.0, 0.5, curve: Curves.easeIn),
     ));
@@ -52,14 +57,24 @@ class _SwipeScreenState extends State<SwipeScreen> with TickerProviderStateMixin
     _backScale = new Tween(
       begin: 1.0,
       end: 0.0,
-    ).animate(new CurvedAnimation(
+    )
+        .animate(new CurvedAnimation(
       parent: _scaleController,
       curve: new Interval(0.0, 0.5, curve: Curves.easeIn),
     ));
 
-    
+    _frontScale.addListener(() {
+      setState(() {});
+    });
 
+    _backScale.addListener(() {
+      setState(() {});
+    });
 
+    _backScale.addStatusListener((status) {
+      if (status == AnimationStatus.completed)
+        _showEasyController.reverse()
+    });
   }
 
   @override
