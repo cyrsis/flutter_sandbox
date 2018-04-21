019b402e93e157af7387508b15bb7dc2689e2ec1
Victor <em3888@gmail.com>
2018-04-21 20:30:10 +0800

    Finishe up the Column 1

--- lib/Screen/SilverAppBarScreen.dart
+++ lib/Screen/SilverAppBarScreen.dart
@@ -8,9 +8,10 @@ class SilverAppBarScreen extends StatefulWidget {
 }
 
 class _SilverAppBarScreenState extends State<SilverAppBarScreen> {
-  List<OrderItem> _items =<OrderItem>[
+  List<OrderItem> _items = <OrderItem>[
     new OrderItem(1, 'Mixed Grill', 'Platter', 1, 30.0, 'mixedgrill.jpg'),
-    new OrderItem(2, 'Grill Chicken', 'Swandwich', 2, 10.0, 'chickswandwich.jpg'),
+    new OrderItem(
+        2, 'Grill Chicken', 'Swandwich', 2, 10.0, 'chickswandwich.jpg'),
     new OrderItem(3, 'Fresh Orange Juice', 'Drink', 3, 8.0, 'orangejuice.jpg'),
     new OrderItem(4, 'Fresh Apple Juice', 'Drink', 1, 9.0, 'applejuice.jpg')
   ];
@@ -32,21 +33,52 @@ class _SilverAppBarScreenState extends State<SilverAppBarScreen> {
           ),
         ),
         new SliverPadding(
-            padding: const EdgeInsets.symmetric(vertical: 2.0),
+          padding: const EdgeInsets.symmetric(vertical: 2.0),
           sliver: new SliverFixedExtentList(
-              delegate: new SliverChildBuilderDelegate(
-                      (builder,index )=>
-    _buildListItem(_items[index])),
-              itemExtent: null),
-
+            delegate: new SliverChildBuilderDelegate(
+              (builder, index) => _buildListItem(_items[index]),
+              childCount: _items.length,
+            ),
+            itemExtent: 172.0,
+          ),
         ),
-
-
       ],
     );
   }
 
   _buildListItem(OrderItem item) {
+    return new Card(
+        child: new Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        new Flexible(
+          child: _buildColum1(item),
+          flex: 1,
+        ),
+        new Flexible(
+          child: _buildColum2(item),
+          flex: 3,
+        ),
+      ],
+    ));
+  }
 
+  _buildColum1(OrderItem item) {
+    return new Column(children: <Widget>[
+      new Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: new InkWell(
+            child: new Hero(
+          tag: item.id,
+          child: new Image.asset('assets/${item.icon}',
+              width: 100.0, height: 100.0, alignment: Alignment.center),
+        )),
+      ),
+    ]);
+  }
+
+  _buildColum2(OrderItem item) {
+    return null;
   }
 }
