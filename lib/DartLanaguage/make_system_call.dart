1cfc55693c4b4d489b6cc6ed5e31f488ad38abf3
cyrsis <em3888@gmail.com>
2018-05-07 18:10:38 +0800

    Working on the lifeCycle

--- lib/DartLanaguage/make_system_call.dart
+++ lib/DartLanaguage/make_system_call.dart
@@ -1,8 +1,3 @@
 main() {
-  var visa_in_text = new RegExp(r"\b4[0-9]{12}(?:[0-9]{3})?\b");
-  var text = "Does this text mention a VISA 4457418557635128 number?";
-  var matches = visa_in_text.allMatches(text);
-  for (var m in matches) {
-    print(m.group(0));
-  }
+  sum(nums) => nums.reduce((a, b) => a + b);
 }
