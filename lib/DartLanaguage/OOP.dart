d23dfd85a550a4b97b403dd4068c67db11f48597
cyrsis <em3888@gmail.com>
2018-06-13 18:00:29 +0800

    Add some oop for outstanding of dart

---
+++ lib/DartLanaguage/OOP.dart
@@ -0,0 +1,44 @@
+main() {
+  var chair = new Chair("padded", 3);
+  print("Here is the chair ${chair.name} & age is ${chair.age}");
+}
+
+class Chair extends Thing {
+  Chair(String name, int age): super(name,age);
+
+  // TODO: implement color
+  @override
+  String get color => "Blue";
+}
+
+abstract class Thing {
+  String name;
+  int age;
+
+  Thing(this.name, this.age);
+
+  String get color;
+
+}
+
+class A {
+  int a;
+  int b;
+  
+}
+
+class B {
+  int c;
+}
+
+class C implements A,B {
+  @override
+  int a;
+
+  @override
+  int b;
+
+  @override
+  int c;
+
+}
