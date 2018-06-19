8b630d7d107ec2216b79a615b9119279b69590ba
cyrsis <em3888@gmail.com>
2018-06-19 17:37:49 +0800

    Adding those model types for good

--- lib/Widgets/PageDragger.dart
+++ lib/Widgets/PageDragger.dart
@@ -8,6 +8,8 @@ class PageDragger extends StatefulWidget {
   final bool canDragRightToLeft;
   final StreamController<SlideUpdate> slideUpdateStream;
 
+  const PageDragger({Key key, this.canDragLeftToRight, this.canDragRightToLeft, this.slideUpdateStream}) : super(key: key);
+
   @override
   _PageDraggerState createState() => new _PageDraggerState();
 }
@@ -16,10 +18,23 @@ class _PageDraggerState extends State<PageDragger> {
 
    static const FULL_TRANSACTION_PX =300.0;
 
-   
+
 
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return new GestureDetector(
+      onHorizontalDragStart: _onDragStart,
+      onHorizontalDragUpdate: _onDragUpdate,
+      onHorizontalDragEnd: _onDragEnd,
+    );
+  }
+
+  void _onDragStart(DragStartDetails details) {
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+  }
+
+  void _onDragEnd(DragEndDetails details) {
   }
 }
