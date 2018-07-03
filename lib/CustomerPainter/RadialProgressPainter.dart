dbb2d6a205dd1e786ab7b9bc4e658f932f9be2a9
Victor <em3888@gmail.com>
2018-07-03 09:16:04 +0800

    Working on the Custom Painter

---
+++ lib/CustomerPainter/RadialProgressPainter.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class RadialProgressPainter extends CustomPainter {
+  double width;
+
+  MaterialColor lineColor;
+
+  MaterialAccentColor completeColor;
+
+  double completePercent;
+
+  RadialProgressPainter(
+      {MaterialColor this.lineColor,
+      MaterialAccentColor this.completeColor,
+      double this.completePercent,
+      double this.width});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    Paint line = new Paint()
+      ..color = lineColor
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = width;
+
+    Paint complete = new Paint()
+      ..color = completeColor
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = width;
+
+    Offset center  = new Offset(size.width/2, size.height/2);
+    double radius  = min(size.width/2,size.height/2);
+    canvas.drawCircle(
+        center,
+        radius,
+        line
+    );
+    double arcAngle = 2*pi* (completePercent/100);
+
+    canvas.drawArc(
+        new Rect.fromCircle(center: center,radius: radius),
+        -pi/2,
+        arcAngle,
+        false,
+        complete
+    );
+  }
+
+  bool shouldRepaint(RadialProgressPainter oldDelegate) => true;
+
+  bool shouldRebuildSemantics(RadialProgressPainter oldDelegate) => false;
+}
--- lib/Screen/TestScreen.dart
+++ lib/Screen/TestScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/CustomerPainter/RadialProgressPainter.dart';
 
 class TestScreen extends StatefulWidget {
   @override
@@ -6,6 +7,8 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
+  var percent =20.0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -13,16 +16,24 @@ class _TestScreenState extends State<TestScreen> {
         new Expanded(
           child: AspectRatio(
             aspectRatio: 1.0,
-            child: new RaisedButton(
-              padding: EdgeInsets.all(20.0),
-              color: Theme.of(context).accentColor,
-              elevation: 4.0,
-              shape: new CircleBorder(),
-              splashColor: Colors.blueGrey,
-              child: const Text('Click me'),
-              onPressed: () {
-                //todo
-              },
+            child: new CustomPaint(
+              painter:new  RadialProgressPainter(
+                lineColor:Colors.amber,
+                completeColor:Colors.blueAccent,
+                completePercent:percent,
+                width:8.0,
+              ),
+              child: new RaisedButton(
+                padding: EdgeInsets.all(20.0),
+                color: Theme.of(context).accentColor,
+                elevation: 4.0,
+                shape: new CircleBorder(),
+                splashColor: Colors.blueGrey,
+                child: const Text('Click me'),
+                onPressed: () {
+                  //todo
+                },
+              ),
             ),
           ),
         ),
