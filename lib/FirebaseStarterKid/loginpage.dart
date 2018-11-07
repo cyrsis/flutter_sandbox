59d6e1927474466d7b2d0bc9eda0be6bf8970e72
Victor <em3888@gmail.com>
2018-11-07 10:00:02 +0800

    I think it is working stage

--- lib/FirebaseStarterKid/loginpage.dart
+++ lib/FirebaseStarterKid/loginpage.dart
@@ -64,169 +64,171 @@ class _LoginPageState extends State<LoginPage> {
     // ));
     return new Scaffold(
         resizeToAvoidBottomPadding: false,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                    child: Text('Hello',
-                        style: TextStyle(
-                            fontSize: 80.0, fontWeight: FontWeight.bold)),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
-                    child: Text('There',
-                        style: TextStyle(
-                            fontSize: 80.0, fontWeight: FontWeight.bold)),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
-                    child: Text('.',
-                        style: TextStyle(
-                            fontSize: 80.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.teal)),
-                  )
-                ],
-              ),
-            ),
-            Container(
-                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: Column(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                child: Stack(
                   children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'EMAIL',
-                        labelStyle: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                      ),
-                      onChanged: (value) {
-                        _email = value;
-                      },
-                    ),
-                    SizedBox(height: 20.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'PASSWORD',
-                        labelStyle: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                      ),
-                      onChanged: (val) {
-                        _password = val;
-                      },
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 5.0),
                     Container(
-                      alignment: Alignment(1.0, 0.0),
-                      padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                      child: InkWell(
-                        child: Text(
-                          'Forgot Password',
+                      padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                      child: Text('Hello',
                           style: TextStyle(
-                              color: Colors.teal,
+                              fontSize: 80.0, fontWeight: FontWeight.bold)),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
+                      child: Text('There',
+                          style: TextStyle(
+                              fontSize: 80.0, fontWeight: FontWeight.bold)),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                      child: Text('.',
+                          style: TextStyle(
+                              fontSize: 80.0,
                               fontWeight: FontWeight.bold,
+                              color: Colors.teal)),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'EMAIL',
+                          labelStyle: TextStyle(
                               fontFamily: 'Montserrat',
-                              decoration: TextDecoration.underline),
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                        ),
+                        onChanged: (value) {
+                          _email = value;
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                        ),
+                        onChanged: (val) {
+                          _password = val;
+                        },
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 5.0),
+                      Container(
+                        alignment: Alignment(1.0, 0.0),
+                        padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                        child: InkWell(
+                          child: Text(
+                            'Forgot Password',
+                            style: TextStyle(
+                                color: Colors.teal,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Montserrat',
+                                decoration: TextDecoration.underline),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 40.0),
-                    Container(
-                      height: 40.0,
-                      child: Material(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.teal,
-                        elevation: 7.0,
-                        child: GestureDetector(
-                          onTap: () {
-                            FirebaseAuth.instance
-                                .signInWithEmailAndPassword(
-                                    email: _email, password: _password)
-                                .then((FirebaseUser user) {
-                              Navigator
-                                  .of(context)
-                                  .pushReplacementNamed('/homepage');
-                            }).catchError((e) {
-                              print(e);
-                            });
-                          },
-                          child: Center(
-                            child: Text(
-                              'LOGIN',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat'),
+                      SizedBox(height: 40.0),
+                      Container(
+                        height: 40.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.teal,
+                          elevation: 7.0,
+                          child: GestureDetector(
+                            onTap: () {
+                              FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                      email: _email, password: _password)
+                                  .then((FirebaseUser user) {
+                                Navigator
+                                    .of(context)
+                                    .pushReplacementNamed('/homepage');
+                              }).catchError((e) {
+                                print(e);
+                              });
+                            },
+                            child: Center(
+                              child: Text(
+                                'LOGIN',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20.0),
-                    Container(
-                      height: 40.0,
-                      color: Colors.transparent,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.black,
-                                style: BorderStyle.solid,
-                                width: 1.0),
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20.0)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            // Center(
-                            //   child:
-                            //       ImageIcon(AssetImage('assets/facebook.png')),
-                            // ),
-                            // SizedBox(width: 10.0),
-                            Center(
-                              child: Text('Log in with facebook',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Montserrat')),
-                            )
-                          ],
+                      SizedBox(height: 20.0),
+                      Container(
+                        height: 40.0,
+                        color: Colors.transparent,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.black,
+                                  style: BorderStyle.solid,
+                                  width: 1.0),
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20.0)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              // Center(
+                              //   child:
+                              //       ImageIcon(AssetImage('assets/facebook.png')),
+                              // ),
+                              // SizedBox(width: 10.0),
+                              Center(
+                                child: Text('Log in with facebook',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Montserrat')),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    )
-                  ],
-                )),
-            SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'New to Spotify ?',
-                  style: TextStyle(fontFamily: 'Montserrat'),
-                ),
-                SizedBox(width: 5.0),
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).pushNamed('/signup');
-                  },
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                        color: Colors.teal,
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline),
+                      )
+                    ],
+                  )),
+              SizedBox(height: 15.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'New to Spotify ?',
+                    style: TextStyle(fontFamily: 'Montserrat'),
                   ),
-                )
-              ],
-            )
-          ],
+                  SizedBox(width: 5.0),
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/signup');
+                    },
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ));
   }
 }
--- lib/FirebaseStarterKid/selectprofpic.dart
+++ lib/FirebaseStarterKid/selectprofpic.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 
-
-
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
